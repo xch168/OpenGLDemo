1bf344515ea6b377234c5543fb1fe4e917e5ec2f
辉天神龙 <122565648@qq.com>
2019-05-30 07:58:44 +0800

    init commit

---
+++ app/src/main/java/com/github/xch168/opengldemo/GLRenderer.java
@@ -0,0 +1,87 @@
+package com.github.xch168.opengldemo;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by XuCanHui on 2019-05-30.
+ */
+public class GLRenderer implements GLSurfaceView.Renderer {
+
+    private static final String VERTEX_SHADER =
+            "attribute vec4 vPosition;\n"
+            + "void main() { \n"
+            + "  gl_Position = vPosition;\n"
+            + "}";
+
+    private static final String FRAGMENT_SHADER =
+            "precision mediump float;\n"
+            + "void main() {\n"
+            + "  gl_FragColor = vec4(0.5, 0, 0, 1);\n"
+            + "}";
+
+    private static final float[] VERTEX = {
+        0, 1, 0,
+        -0.5f, -1, 0,
+        1, -1, 0
+    };
+
+    private final FloatBuffer mVertexBuffer;
+
+    private int mProgram;
+    private int mPositionHandler;
+
+    public GLRenderer() {
+        mVertexBuffer = ByteBuffer.allocateDirect(VERTEX.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(VERTEX);
+        mVertexBuffer.position(0);
+    }
+
+    static int loadShader(int type, String shaderCode) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+        return shader;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        mProgram = GLES20.glCreateProgram();
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
+        GLES20.glAttachShader(mProgram, vertexShader);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        GLES20.glLinkProgram(mProgram);
+
+        GLES20.glUseProgram(mProgram);
+
+        mPositionHandler = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        GLES20.glEnableVertexAttribArray(mPositionHandler);
+        GLES20.glVertexAttribPointer(mPositionHandler, 3, GLES20.GL_FLOAT, false,
+                12, mVertexBuffer);
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+    }
+}
---
+++ app/src/main/java/com/github/xch168/opengldemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.github.xch168.opengldemo;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGlSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGlSurfaceView = findViewById(R.id.gl_sv);
+        mGlSurfaceView.setEGLContextClientVersion(2);
+        mGlSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        mGlSurfaceView.setRenderer(new GLRenderer());
+        mGlSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGlSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGlSurfaceView.onPause();
+    }
+
+}
